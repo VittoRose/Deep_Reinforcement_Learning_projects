56e0e6c1bd97d4aa9eaf0010392866a57fe29f95
VittoRose <135705006+VittoRose@users.noreply.github.com>
2024-10-20 13:28:51 +0200

    Test

--- GYM_test_PPO/md_report.py
+++ GYM_test_PPO/md_report.py
@@ -10,17 +10,20 @@ def create_md_summary(gym_id: str, name: str)-> None:
     
     with open(report, 'w') as file:
         file.write("# Enviroment: " + gym_id + "\n")
-        file.write("# Training parameters\n")
+        file.write("## Training parameters\n")
 
-        file.write(f"-Number of enviroments: {n_env}\n")
-        file.write(f"-Timestep for collecting data T = {n_step}\n")
-        file.write(f"-Total data for each loop: {BATCH_SIZE}\n")
-        file.write(f"-Update epoch K = {K_EPOCHS}\n")
-        file.write(f"-Minibatch size {MINI_BATCH_SIZE}\n\n")
+        file.write(f"- Number of enviroments: {n_env}\n")
+        file.write(f"- Timestep for collecting data T = {n_step}\n")
+        file.write(f"- Total data for each loop: {BATCH_SIZE}\n")
+        file.write(f"- Update epoch K = {K_EPOCHS}\n")
+        file.write(f"- Minibatch size {MINI_BATCH_SIZE}\n\n")
 
-        file.write("# Hyperparameters\n")
-        file.write(f"*Discount factor: {GAMMA}\n")
-        file.write(f"*GAE lambda: {GAE_LAMBDA}\n")
-        file.write(f"*Learning rate: {LR}\n")
-        file.write(f"*Clipping factor: {CLIP}\n")
-        file.write(f"*Loss c1 = {VALUE_COEFF} c2 = {ENTROPY_COEF}\n")
+        file.write("## Hyperparameters\n")
+        file.write(f"* Discount factor: {GAMMA}\n")
+        file.write(f"* GAE lambda: {GAE_LAMBDA}\n")
+        file.write(f"* Learning rate: {LR}\n")
+        file.write(f"* Clipping factor: {CLIP}\n")
+        file.write(f"* Loss c1 = {VALUE_COEFF} c2 = {ENTROPY_COEF}\n")
+
+if __name__ == "__main__":
+    create_md_summary("Summary test", " Testissimo")
