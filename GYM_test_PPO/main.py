5f0c4c8bdf08fbd963235701eecf5f491af10860
VittoRose <135705006+VittoRose@users.noreply.github.com>
2024-10-25 14:12:54 +0200

    Add keyboard interrupt

--- GYM_test_PPO/main.py
+++ GYM_test_PPO/main.py
@@ -58,120 +58,126 @@ next_done = torch.zeros(n_env)
                     TRAINING LOOP
 ------------------------------------------------------------
 """
+epoch, running = 0, True
+try:
+    while epoch < MAX_EPOCH and running:
 
-for epoch in range(0, MAX_EPOCH):
+        # Show progress during training
+        logger.show_progress(epoch)
 
-    # Show progress during training
-    logger.show_progress(epoch)
+        test_network(epoch, agent, test_env, logger)
+        
+        # Here we can modify the learning rate
 
-    test_network(epoch, agent, test_env, logger)
-    
-    # Here we can modify the learning rate
+        # Collect data from the enviroment
+        for step in range(0, n_step):
 
-    # Collect data from the enviroment
-    for step in range(0, n_step):
+            obs[step] = next_obs
+            dones[step] = next_done
 
-        obs[step] = next_obs
-        dones[step] = next_done
+            # Select an action
+            with torch.no_grad():
+                action, logprob, _, value = agent.get_action_and_value(next_obs)
+                values[step] = value.flatten()
+            actions[step] = action
+            logprobs[step] = logprob
 
-        # Select an action
+            # Execute action in enviroment
+            next_obs, reward, truncated, terminated, _ = envs.step(action.numpy())
+            done = terminated | truncated
+
+            rewards[step] = torch.tensor(reward)
+            next_obs, next_done = torch.tensor(next_obs), torch.tensor(done)
+
+            # Collect rewards per episode
+            with torch.no_grad():
+                ep_reward = ep_reward + rewards[step]
+
+            for i, cut in enumerate(done):
+                if cut:
+                    logger.add_train_rew(ep_reward[i])
+                    ep_reward[i] = 0
+
+        # bootstrap value if not done
         with torch.no_grad():
-            action, logprob, _, value = agent.get_action_and_value(next_obs)
-            values[step] = value.flatten()
-        actions[step] = action
-        logprobs[step] = logprob
+            next_value = agent.get_value(next_obs).reshape(1, -1)
+            advantages = torch.zeros_like(rewards)
+            lastgaelam = 0
+            for t in reversed(range(n_step)):
+                if t == n_step- 1:
+                    nextnonterminal = 1.0 - next_done.int()
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - dones[t + 1].int()
+                    nextvalues = values[t + 1]
+                delta = rewards[t] + GAMMA * nextvalues * nextnonterminal - values[t]
+                advantages[t] = lastgaelam = delta + GAMMA * GAE_LAMBDA * nextnonterminal * lastgaelam
+            returns = advantages + values
 
-        # Execute action in enviroment
-        next_obs, reward, truncated, terminated, _ = envs.step(action.numpy())
-        done = terminated | truncated
+        # flatten the batch
+        b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = logprobs.reshape(-1)
+        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        b_advantages = advantages.reshape(-1)
+        b_returns = returns.reshape(-1)
+        b_values = values.reshape(-1)
 
-        rewards[step] = torch.tensor(reward)
-        next_obs, next_done = torch.tensor(next_obs), torch.tensor(done)
+        index = np.arange(BATCH_SIZE)
 
-        # Collect rewards per episode
-        with torch.no_grad():
-            ep_reward = ep_reward + rewards[step]
+        # Update network K times
+        for epoch in range(K_EPOCHS):
 
-        for i, cut in enumerate(done):
-            if cut:
-                logger.add_train_rew(ep_reward[i])
-                ep_reward[i] = 0
+            # Shuffle index to break correlations
+            np.random.shuffle(index)
 
-    # bootstrap value if not done
-    with torch.no_grad():
-        next_value = agent.get_value(next_obs).reshape(1, -1)
-        advantages = torch.zeros_like(rewards)
-        lastgaelam = 0
-        for t in reversed(range(n_step)):
-            if t == n_step- 1:
-                nextnonterminal = 1.0 - next_done.int()
-                nextvalues = next_value
-            else:
-                nextnonterminal = 1.0 - dones[t + 1].int()
-                nextvalues = values[t + 1]
-            delta = rewards[t] + GAMMA * nextvalues * nextnonterminal - values[t]
-            advantages[t] = lastgaelam = delta + GAMMA * GAE_LAMBDA * nextnonterminal * lastgaelam
-        returns = advantages + values
+            # Update using minibatches
+            for start in range(0, BATCH_SIZE, MINI_BATCH_SIZE):     
+                
+                # Select the index for minibatch
+                end = start + MINI_BATCH_SIZE
+                mini_batch_index = index[start:end]
 
-    # flatten the batch
-    b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
-    b_logprobs = logprobs.reshape(-1)
-    b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
-    b_advantages = advantages.reshape(-1)
-    b_returns = returns.reshape(-1)
-    b_values = values.reshape(-1)
+                _, newlogprob, entropy, newval = agent.get_action_and_value(b_obs[mini_batch_index], b_actions.long()[mini_batch_index])
+                logratio = newlogprob - b_logprobs[mini_batch_index]
+                ratio = logratio.exp()
 
-    index = np.arange(BATCH_SIZE)
+                mb_advantages = b_advantages[mini_batch_index]
 
-    # Update network K times
-    for epoch in range(K_EPOCHS):
+                # Normalize advantages
+                mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
+                
+                # Policy loss
+                surr1 = -mb_advantages * ratio
+                surr2 = -mb_advantages * torch.clamp(ratio, 1-CLIP, 1+CLIP)
+                policy_loss = torch.max(surr1, surr2).mean()
 
-        # Shuffle index to break correlations
-        np.random.shuffle(index)
+                # Value loss
+                if VALUE_CLIP:
+                    v_clip = b_values[mini_batch_index] + torch.clamp(newval.squeeze()-b_values[mini_batch_index], 1-CLIP, 1+CLIP)
+                    v_losses = torch.nn.functional.mse_loss(newval.squeeze(), b_returns[mini_batch_index])
+                    v_loss_max = torch.max(v_clip, v_losses)
+                    v_loss = 0.5*v_loss_max.mean()
+                else:
+                    v_losses = torch.nn.functional.mse_loss(newval.squeeze(), b_returns[mini_batch_index])
+                    v_loss = v_losses.mean()
 
-        # Update using minibatches
-        for start in range(0, BATCH_SIZE, MINI_BATCH_SIZE):     
-            
-            # Select the index for minibatch
-            end = start + MINI_BATCH_SIZE
-            mini_batch_index = index[start:end]
+                # Entropy loss
+                entropy_loss = entropy.mean()
 
-            _, newlogprob, entropy, newval = agent.get_action_and_value(b_obs[mini_batch_index], b_actions.long()[mini_batch_index])
-            logratio = newlogprob - b_logprobs[mini_batch_index]
-            ratio = logratio.exp()
+                # Global loss function
+                loss = policy_loss - ENTROPY_COEF*entropy_loss + VALUE_COEFF*v_loss
+                logger.add_loss(loss.item())
 
-            mb_advantages = b_advantages[mini_batch_index]
+                # Backpropagation
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(agent.parameters(), 0.5)
+                optimizer.step()
 
-            # Normalize advantages
-            mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
-            
-            # Policy loss
-            surr1 = -mb_advantages * ratio
-            surr2 = -mb_advantages * torch.clamp(ratio, 1-CLIP, 1+CLIP)
-            policy_loss = torch.max(surr1, surr2).mean()
-
-            # Value loss
-            if VALUE_CLIP:
-                v_clip = b_values[mini_batch_index] + torch.clamp(newval.squeeze()-b_values[mini_batch_index], 1-CLIP, 1+CLIP)
-                v_losses = torch.nn.functional.mse_loss(newval.squeeze(), b_returns[mini_batch_index])
-                v_loss_max = torch.max(v_clip, v_losses)
-                v_loss = 0.5*v_loss_max.mean()
-            else:
-                v_losses = torch.nn.functional.mse_loss(newval.squeeze(), b_returns[mini_batch_index])
-                v_loss = v_losses.mean()
-
-            # Entropy loss
-            entropy_loss = entropy.mean()
-
-            # Global loss function
-            loss = policy_loss - ENTROPY_COEF*entropy_loss + VALUE_COEFF*v_loss
-            logger.add_loss(loss.item())
-
-            # Backpropagation
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(agent.parameters(), 0.5)
-            optimizer.step()
+                # Update epoch
+                epoch +=1
+except KeyboardInterrupt:
+    running = False
 
 
 # Close api
